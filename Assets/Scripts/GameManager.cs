b6ca8de71875311f74d591a6fe7d1db3cc64bd06
tkpleo <tkristofferpurdy@gmail.com>
2026-04-20 13:21:00 -0400

    timescale set in awake method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,12 +14,28 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOverTime;
 
     //enable gameover screen when player dies
+
+    private void Awake()
+    {
+        // Ensure the game is unpaused whenever a scene containing this GameManager loads.
+        if (Time.timeScale != 1f)
+        {
+            Debug.Log("[GameManager] Awake resetting Time.timeScale from " + Time.timeScale + " to 1f");
+            Time.timeScale = 1f;
+        }
+    }
     public void GameOver()
     {
         Time.timeScale = 0f;
         gameOverScreen.SetActive(true);
         gameOverTime.text = timerText.text + " seconds!";
         timerText.gameObject.SetActive(false);
+    }
+
+    public void RetartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("GameplayScene");
     }
 
     public void pause()
--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -6,6 +6,10 @@ public class MenuScript : MonoBehaviour
 
     public void StartGame()
     {
+        if (Time.timeScale != 1f)
+        { 
+            Time.timeScale = 1f;
+        }
         SceneManager.LoadScene("GameplayScene");
     }
 
